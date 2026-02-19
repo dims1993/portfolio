5eeae7132f891b42babc3db8eec7e5b7e20e111d
David <dimsepp@gmail.com>
2026-02-19 13:41:35 +0100

    Comment: Changes to adapt the new Head in order to unify it with main after (Testing)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,12 +4,14 @@ import Hero from "../sections/Hero";
 import Projects from "../sections/Projects";
 import Skills from "../sections/Skills";
 import Contact from "../sections/Contact";
+import About from "../sections/About";
 
 function App() {
   return (
     <Layout>
       <Navbar />
       <Hero />
+      <About />
       <Projects />
       <Skills />
       <Contact />
